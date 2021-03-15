5f2cf179268f8574a2ff962505d71aa477b850b0
EC2 Default User <ec2-user@ip-172-31-13-221.us-east-2.compute.internal>
2021-03-15 04:48:45 +0000

    part 3

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 
 
-
-describe 'Search movies by the same director' do
-    it 'should call Movie.similar_movies' do
-      
+describe MoviesController do
+    describe 'Searching for movies with the same director' do
+        
+        it 'should return to the homepage if no director infor exist' do
+            
+        end   
+        
+        it 'should search for similar_movies if director given' do
+            movie_01 = Movie.create!(title: 'iRobot', director: 'Alex Proyas')
+            movie_02 = Movie.create!(title: 'Joy', director: 'Alex Proyas')
+            #get :search_similar_movies, {title: 'Star Wars'}
+            #expect((@similar_movies)).to eq ['Star Wars', 'THX-1138']
+            expect((@similar_movies)).to eq nil
+        end
+        
+        #it "should display 'THX-1138' for 'Star Wars'" do
+         #end
+        
     end
 end
